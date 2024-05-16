3bd04dc5c7726ec1513e06c5b6250c56a716f1cb
Yahya Parvar <yahyaparvar1@gmail.com>
2024-05-16 17:56:31 +0330

    login with oauth

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,21 @@
-import express, { Request, Response } from 'express';
-import axios from 'axios';
-import bodyParser from 'body-parser';
+import axios from "axios";
+import bodyParser from "body-parser";
+import cors from "cors";
+import dotenv from "dotenv";
+import express, { Request, Response } from "express";
+
+dotenv.config(); // Load environment variables from .env file
 
 const app = express();
 app.use(bodyParser.json());
+
+const corsOpts = {
+  origin: "*",
+
+  methods: ["GET", "POST"],
+};
+
+app.use(cors(corsOpts));
 
 interface GoogleAuthRequest extends Request {
   body: {
@@ -11,19 +23,23 @@ interface GoogleAuthRequest extends Request {
   };
 }
 
-app.post('/auth/google', async (req: GoogleAuthRequest, res: Response) => {
+app.post("/auth/google", async (req: GoogleAuthRequest, res: Response) => {
   const { code } = req.body;
 
   try {
-    const response = await axios.post('https://oauth2.googleapis.com/token', null, {
-      params: {
-        code,
-        client_id: 'YOUR_GOOGLE_CLIENT_ID',
-        client_secret: 'YOUR_GOOGLE_CLIENT_SECRET',
-        redirect_uri: 'YOUR_REDIRECT_URI',
-        grant_type: 'authorization_code',
-      },
-    });
+    const response = await axios.post(
+      "https://oauth2.googleapis.com/token",
+      null,
+      {
+        params: {
+          code,
+          client_id: process.env.GOOGLE_CLIENT_ID,
+          client_secret: process.env.GOOGLE_CLIENT_SECRET,
+          redirect_uri: process.env.REDIRECT_URI,
+          grant_type: "authorization_code",
+        },
+      }
+    );
 
     res.json(response.data);
   } catch (error: any) {
