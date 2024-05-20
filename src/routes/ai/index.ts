4f607265c5e045ffb683f2994c0386e951cfc300
Yahya Parvar <yahyaparvar1@gmail.com>
2024-05-20 20:46:43 +0330

    fixed AI

--- src/routes/ai/index.ts
+++ src/routes/ai/index.ts
@@ -1,23 +1,23 @@
-import axios from "axios";
+import dotenv from "dotenv";
 import express from "express";
+import OpenAI from "openai";
 
+dotenv.config();
+
+const openai = new OpenAI({
+  apiKey: process.env.OPENAI_API_KEY,
+});
 export const aiRouter = express.Router();
 aiRouter.post("/emails/summary", async (req, res) => {
-  const { prompt } = req.body;
-
+  const { prompt: userPrompt } = req.body;
+  const prompt = `this is my user input ${userPrompt}`;
   try {
-    const response = await axios.post(
-      "https://api-inference.huggingface.co/models/gpt2",
-      { inputs: prompt },
-      {
-        headers: {
-          Authorization: `Bearer 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`,
-          "Content-Type": "application/json",
-        },
-      }
-    );
+    const gptResponse = await openai.chat.completions.create({
+      messages: [{ role: "system", content: prompt }],
+      model: "gpt-3.5-turbo",
+    });
 
-    res.json(response.data);
+    res.json(gptResponse);
   } catch (error) {
     console.error("Error fetching AI response:", error);
     res.status(500).json({ error: error });
